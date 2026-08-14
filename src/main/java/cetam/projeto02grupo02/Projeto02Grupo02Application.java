aabbfbd9e62e3f8ed3aa3a54e67765332c1c67d1
Cleverson S. Sobrinho <cleverson.silva@msn.com>
2026-08-13 22:51:44 -0400

    Atualização do application.properties

--- src/main/java/cetam/projeto02grupo02/Projeto02Grupo02Application.java
+++ src/main/java/cetam/projeto02grupo02/Projeto02Grupo02Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Projeto02Grupo02Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Projeto02Grupo02Application.class, args);
-	}
+    public static void main ( String[] args ) {
+        SpringApplication.run ( Projeto02Grupo02Application.class, args );
+    }
 
 }
